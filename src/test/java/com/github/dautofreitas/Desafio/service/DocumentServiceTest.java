44e64286b8598cf3daef782db3172b7e8f3844c7
Adauto Freitas <c0512913@globalvale.onmicrosoft.com>
2020-06-07 15:54:27 -0300

    Atualização Spring boot

--- src/test/java/com/github/dautofreitas/Desafio/service/DocumentServiceTest.java
+++ src/test/java/com/github/dautofreitas/Desafio/service/DocumentServiceTest.java
@@ -1,13 +1,11 @@
 package com.github.dautofreitas.Desafio.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.lang.reflect.Method;
 import java.util.Optional;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
